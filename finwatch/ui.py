2122c80008a54b1f6bd16f43259300569d373039
Andre Gil <thomasgil913@gmail.com>
2026-02-20 09:47:50 -0500

    Improve getting renderable objects

--- finwatch/ui.py
+++ finwatch/ui.py
@@ -23,14 +23,14 @@ def _print_table(table: Table) -> None:
     console.print(table)
 
 
-def _get_renderable(dump: ModelDump) -> dict[str, str]:
+def _get_renderable_data(dump: ModelDump):
     renderable_data = {}
     for att, value in dump.items():
         if isinstance(value, datetime):
             renderable_data[att] = str(value.date())
             continue
         renderable_data[att] = str(value)
-    return renderable_data
+    return renderable_data.values()
 
 
 def render_lookup_symbols(symbols: list[LookupSymbol]):
@@ -40,8 +40,8 @@ def render_lookup_symbols(symbols: list[LookupSymbol]):
 
     table = _get_table_header(symbols[0].model_dump())
     for symbol in symbols:
-        renderable_obj = _get_renderable(symbol.model_dump())
-        table.add_row(*renderable_obj.values())
+        renderables = _get_renderable_data(symbol.model_dump())
+        table.add_row(*renderables)
     _print_table(table)
 
 
@@ -51,6 +51,6 @@ def render_quote(
 ) -> None:
     data = quote.model_dump(exclude={"percent_change"})
     table = _get_table_header(data, title=f"Quote for {symbol}")
-    renderable_obj = _get_renderable(data)
-    table.add_row(*renderable_obj.values())
+    renderables = _get_renderable_data(data)
+    table.add_row(*renderables)
     _print_table(table)
